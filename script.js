5f2ed695d4f1d5eb60455fad066cd23ae78db5bb
Syed ubaid <syedabuubaid@gmail.com>
2023-09-03 00:18:58 +0530

    js changes

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
+computerSelection=0
+playerChoice=0
+
+
 function getcomputerChoice(){
 
     let generatedNumber=Math.floor(Math.random()*4)
-    if (generatedNumber=1){
+    if (generatedNumber==1){
         computerSelection="rock";
     }
-    if (generatedNumber=2){
+    if (generatedNumber==2){
         computerSelection="paper";
     }    
-    if (generatedNumber=3){
+    if (generatedNumber==3){
         computerSelection="scissor";
     }
     return computerSelection;
+}
+
+function playerSelection(){
+    var playerChoice=prompt("Enter your choice (R,P,S)")
+    return playerChoice;
+
+}
+
+function playround(getcomputerChoice,playerSelection){
+    if (getcomputerChoice=="scissor"){
+        if (playerChoice=="R")
+        return("its a draw");   
+}
+}
+
+function test(getcomputerChoice){
+
 }
